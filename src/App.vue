f5d0d4f60de33c77a7b9ff3684e6f1b1674d9400
ckh35 <ckh35@uclive.ac.nz>
2022-01-03 10:46:14 +1300

    create link to side bar, and make the about page

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
     <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h6"> Application </v-list-item-title>
-          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+          <v-list-item-title class="text-h6"> Vuetify Todo </v-list-item-title>
+          <v-list-item-subtitle> Christopher Kevin Hamdajani </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to"  link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -31,7 +31,7 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -41,8 +41,8 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-      { title: "Dashboard", icon: "mdi-view-dashboard" },
-      { title: "About", icon: "mdi-help-box" },
+      { title: "Todo", icon: "mdi-format-list-checks", to:"/" },
+      { title: "About", icon: "mdi-help-box", to:"/about" },
     ],
   }),
 };
